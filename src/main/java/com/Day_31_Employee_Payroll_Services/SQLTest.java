a3a94587cea0c1f3affbe7df5f53686ec1279089
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-12-01 15:48:35 +0530

    Added: Files With UC1

--- src/main/java/com/Day_31_Employee_Payroll_Services/SQLTest.java
+++ src/main/java/com/Day_31_Employee_Payroll_Services/SQLTest.java
@@ -9,25 +9,11 @@ public class SQLTest
         String jdbcurl="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
         String userName="root";
         String password="Rehana@786";
-        Connection con;
-        Statement statement ;
+        Connection connection;        
         try{
             //Class.forName("com.mysql.jdbc.Driver");
         	Class.forName("com.mysql.cj.jdbc.Driver");
-        	System.out.println("Driver Loaded");
-            con= DriverManager.getConnection(jdbcurl, userName, password);
-            statement= con.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
-            while(resultSet.next())
-            {
-				int id = resultSet.getInt("id");
-				String name = resultSet.getString("name");
-				String basic_pay = resultSet.getString("basic_pay");
-				System.out.print(id + " ");
-				System.out.print(name + " ");
-				System.out.print(basic_pay + " ");
-				System.out.println(" ");
-            }
+        	System.out.println("Driver Loaded!");            
         }
         catch (Exception e)
         {
